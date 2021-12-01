590b1964ba4c01ebe30ac587b4593612f780b219
archi2811 <archi.mamaniya@somaiya.edu>
2021-12-01 17:32:48 +0530

    Vendors

---
+++ lasoiree/lib/food_data.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FoodData {
+  static final getData = [
+    {
+      'name': 'Basil and Spice',
+      'image':
+          'http://www.bangalorecaterers.com/listingo/wp-content/uploads/2013/12/1.jpg',
+      'location': 'Thane',
+      'ratings': 4
+    },
+    {
+      'name': 'Taste Studio',
+      'image':
+          'https://www.sscaterers.com/assets/image-path/corporate-catering/2.jpg',
+      'location': 'Powai',
+      'ratings': 4
+    },
+    {
+      'name': 'La Cuisine',
+      'image':
+          'https://img.weddingbazaar.com/assets/vendor/55049/wm/638X3630378396001629347497.webp',
+      'location': 'Thane',
+      'ratings': 2
+    },
+  ];
+}
